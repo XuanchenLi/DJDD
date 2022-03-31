ddfeb5c6562c1b614d886a25aafb4bdd817dcbc1
Dazz1e <2468691031@qq.com>
2022-03-31 12:18:27 +0800

    initial

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import torch
+import torch.nn as nn
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class JDDNetwork(nn.model):
+    def __init__(self, width, depth, noise_level, pre_trained=False, padding=True):
+        super(JDDNetwork, self).__init__()
 
+        self.width = width
+        self.depth = depth
+        self.noise_level = noise_level
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        
